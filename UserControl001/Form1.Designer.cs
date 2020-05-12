f2ef556fd6c1ebd495d6bb4e032874e7c61e2d89
gitmsx <47451157+gitmsx@users.noreply.github.com>
2020-05-12 11:59:21 +0300

    3 commit

--- UserControl001/Form1.Designer.cs
+++ UserControl001/Form1.Designer.cs
@@ -28,13 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.vad_Label1 = new UserControl001.Vad_Label();
+            this.SuspendLayout();
+            // 
+            // vad_Label1
+            // 
+            this.vad_Label1.Location = new System.Drawing.Point(346, 13);
+            this.vad_Label1.Name = "vad_Label1";
+            this.vad_Label1.Size = new System.Drawing.Size(175, 68);
+            this.vad_Label1.TabIndex = 0;
+            this.vad_Label1.Text = "vad_Label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.vad_Label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Vad_Label vad_Label1;
     }
 }
 
--- UserControl001/Vad_Label.cs
+++ UserControl001/Vad_Label.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -10,6 +13,30 @@ namespace UserControl001
     class Vad_Label : Control
 
     {
+        public Vad_Label()
+        {
+            
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.ResizeRedraw | ControlStyles.SupportsTransparentBackColor | ControlStyles.UserPaint, true);
+            DoubleBuffered = true;
 
+            Size = new Size(100,30); // Egolds video
+
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics graph = e.Graphics;
+            graph.SmoothingMode = SmoothingMode.HighQuality;
+                graph.Clear(Parent.BackColor);
+
+            Rectangle rect = new Rectangle(0, 0, Width-1, Height-1);
+
+            graph.DrawRectangle(new Pen(BackColor),rect);
+
+        }
+
+
+
+    
     }
 }
